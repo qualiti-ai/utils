d121c7b1820e08deb47dde52cd2d4bed96dfbcda
Daniel Hair <danielmhair@gmail.com>
2021-12-20 16:37:30 +0000

    fix: setting up npmrc with only token

--- setup-npmrc.sh
+++ setup-npmrc.sh
@@ -3,17 +3,17 @@
 
 set -e
 
-if [ -z "$GITHUB_USERNAME" ]; then
-  echo ""
-  echo "ERROR: No GITHUB_USERNAME defined in environment variables. Be sure to set your username in Gitpod or in your environment variables before continuing."
-  exit 1
-fi
+# if [ -z "$GITHUB_USERNAME" ]; then
+#   echo ""
+#   echo "ERROR: No GITHUB_USERNAME defined in environment variables. Be sure to set your username in Gitpod or in your environment variables before continuing."
+#   exit 1
+# fi
 
-if [ -z "$GITHUB_EMAIL" ]; then
-  echo ""
-  echo "ERROR: No GITHUB_EMAIL defined in environment variables. Be sure to set your username in Gitpod or in your environment variables before continuing."
-  exit 1
-fi
+# if [ -z "$GITHUB_EMAIL" ]; then
+#   echo ""
+#   echo "ERROR: No GITHUB_EMAIL defined in environment variables. Be sure to set your username in Gitpod or in your environment variables before continuing."
+#   exit 1
+# fi
 
 if [ -z "$GITHUB_TOKEN" ]; then
   echo ""
@@ -21,13 +21,17 @@ if [ -z "$GITHUB_TOKEN" ]; then
   exit 1
 fi
 
-sudo apt-get update && sudo apt-get install expect -yq --no-install-recommends
+npm set @qualiti-ai:registry "https://npm.pkg.github.com"
+npm set registry "https://registry.npmjs.org"
+npm set //npm.pkg.github.com/:_authToken $GITHUB_TOKEN
 
-/usr/bin/expect <<EOD
-  spawn npm login --registry=https://npm.pkg.github.com --scope=@qualiti-ai
-  expect {
-      "Username: " {send "$GITHUB_USERNAME\r"; exp_continue}
-      "Password: " {send "$GITHUB_TOKEN\r"; exp_continue}
-      "Email: (this IS public) " {send "$GITHUB_EMAIL\r"; exp_continue}
-  }
-EOD
+# sudo apt-get update && sudo apt-get install expect -yq --no-install-recommends
+
+# /usr/bin/expect <<EOD
+#   spawn npm login --registry=https://npm.pkg.github.com --scope=@qualiti-ai
+#   expect {
+#       "Username: " {send "$GITHUB_USERNAME\r"; exp_continue}
+#       "Password: " {send "$GITHUB_TOKEN\r"; exp_continue}
+#       "Email: (this IS public) " {send "$GITHUB_EMAIL\r"; exp_continue}
+#   }
+# EOD
