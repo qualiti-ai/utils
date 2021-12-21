d5d663e94ba498505a9f4e166436fc0d758f7fae
Daniel Hair <danielmhair@gmail.com>
2021-12-21 15:37:22 +0000

    fix: update setup-git bug

--- setup-git.sh
+++ setup-git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 
-git config --global user.email "github[bot]@bot.com"
-git config --global user.name "github[bot]"
+git config --global user.email "github-bot@bot.com"
+git config --global user.name "Github Bot"
