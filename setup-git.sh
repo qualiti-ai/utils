9a711834b82da49f1fbce388c9afb4dcacf84fe2
Daniel Hair <danielmhair@gmail.com>
2021-12-21 18:21:46 +0000

    chore: testing

--- setup-git.sh
+++ setup-git.sh
@@ -3,7 +3,8 @@ set -ex
 
 echo "Setting up Git User..."
 
-git config --global user.email "github-bot@bot.com"
-git config --global user.name "Github Bot"
-git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${REPO}.git
+git config --global user.email "${GH_USERNAME}@users.noreply.github.com"
+git config --global user.name "${GH_USERNAME}"
+
+git remote set-url origin https://dhair:${GITHUB_TOKEN}@github.com/${REPO}.git
 echo "Git User is setup!"
