0867ac71edf41aa8d61e84eaffab68aae52a2fe7
Daniel Hair <danielmhair@gmail.com>
2021-12-21 15:50:03 +0000

    fix: update setup-git

--- setup-git.sh
+++ setup-git.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ex
 
+echo "Setting up Git User..."
+
 git config --global user.email "github-bot@bot.com"
 git config --global user.name "Github Bot"
+git remote rm origin
+git remote add origin 'git@github.com:github-bot/${REPO_NAME}.git'
+echo "Git User is setup!"
