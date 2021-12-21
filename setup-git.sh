f96c300d5eede275f4ac907bae413a0a63bae73b
Daniel Hair <danielmhair@gmail.com>
2021-12-21 18:09:11 +0000

    fix: quotes in setup-git

--- setup-git.sh
+++ setup-git.sh
@@ -5,5 +5,5 @@ echo "Setting up Git User..."
 
 git config --global user.email "github-bot@bot.com"
 git config --global user.name "Github Bot"
-git remote set-url origin 'https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${REPO}.git'
+git remote set-url origin https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${REPO}.git
 echo "Git User is setup!"
