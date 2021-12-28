602a9214a72640831d525841a4eb701abd4fe8f9
Derrick Walton <dwalton@qualiti.ai>
2021-12-28 10:51:40 -0700

    fix: user bug

--- setup-git.sh
+++ setup-git.sh
@@ -6,5 +6,5 @@ echo "Setting up Git User..."
 git config --global user.email "${GITHUB_USERNAME}@users.noreply.github.com"
 git config --global user.name "${GITHUB_USERNAME}"
 
-git remote set-url origin https://dhair:${GITHUB_TOKEN}@github.com/${REPO}.git
+git remote set-url origin https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${REPO}.git
 echo "Git User is setup!"
